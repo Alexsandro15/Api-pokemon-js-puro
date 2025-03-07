221fb54ca976cbb9caa8e3eb3cde1d511399918c
Alexsandro15 <alexetemac@gmail.com>
2025-03-07 18:38:17 -0300

    adicionando tratamento de erros

--- script/pokemon.js
+++ script/pokemon.js
@@ -1,126 +1,128 @@
+let erropkm = document.querySelector(".erro");
+let cardpkm = document.querySelector(".card-pokemon");
 async function usandoApiPokemon(nome) {
     try{
-        console.log('Iniciando a requisição...'); 
+        console.log('Iniciando a requisição...');
         
         const apiUrl = `https://pokeapi.co/api/v2/pokemon/${nome}`;
         const response = await fetch(apiUrl);
 
-        
         if (!response.ok) {
             throw new Error('Erro na requisição: ' + response.status);
         }
 
-        
         const data = await response.json();
 
-        console.log("mostrando dados");
-        
         let status = data.stats;
         
         let tipos = data.types;
         let tipospkm = [];
         tipos.forEach(tipo => {
-            console.log("tipo do pokemon ",tipo.type.name);
             tipospkm.push(tipo.type.name);
         });
         
-
-        console.log("tipo: ",tipospkm);
-
-        console.log("status do pokemon", status);
         let statusPkm = [];
 
         status.forEach(stat => {
-            console.log(stat.base_stat);
-            console.log(stat.stat.name);
             let statuspkmvalor = {
                 nomestatus:stat.stat.name, valor: stat.base_stat
             }
             statusPkm.push(statuspkmvalor);
         });
-        console.log("array do pkm", statusPkm);
-        let sprites = data.sprites;
-        let home = sprites.other.home.front_default;
         
-        console.log(home);
+        let sprites = data.sprites;
+        let home = sprites.front_default;
+        
         return [home, statusPkm, tipospkm];
 
     }catch(erro){
-        console.log(`nome do pokemon: ${nome} ñ encontrado`);
+        erropkm.style.color = 'red';
+        erropkm.innerHTML = `pokemon: ${nome} ñ encontrado`;
+        cardpkm.innerHTML = "";
+        cardpkm.classList.remove("card-pokemon");
+        cardpkm.classList.add("erro-card");
+        throw new Error(erro.message);
+    }finally{
+        console.log("requisição finalizada");
     }
 }
 let btn = document.querySelector("#btn");
 btn.addEventListener('click', async function(event) {
 event.preventDefault();
-let nome = document.querySelector("#nomepkm").value;
-nome = nome.toLowerCase().trim();
-let [imagem, arraypkm, tipos] = await usandoApiPokemon(nome);
-console.log("array de fora", arraypkm);
 
-
-if(imagem && arraypkm && tipos){
-    function iniciarAnimacao() {
-        cardpkm.classList.add("giro");
+try{
+    let nome = document.querySelector("#nomepkm").value;
+    if(nome.length == 0){
+        erropkm.innerHTML = `o nome ñ pode ser vazio`;
+        throw new Error('Erro: o nome não pode ser vazio');
     }
+    erropkm.innerHTML = ``;
+
+    nome = nome.toLowerCase().trim();
+    let [imagem, arraypkm, tipos] = await usandoApiPokemon(nome);
     
-    function pararAnimacao() {
-        cardpkm.style.animation = "none"; 
-        cardpkm.offsetHeight; 
-        cardpkm.style.animation = null; 
-    }
-    
+    if(imagem && arraypkm && tipos){
 
-
-let cardpkm = document.querySelector("#card-pokemon");
-cardpkm.style.display = "flex";
-iniciarAnimacao();
-
-function renderizaCard() {
-    console.log("Executando após 1 segundo...");
-    cardpkm.innerHTML = `
-    <div id="infocima">
-        <p id="nome">${nome}</p>
-        <div id="tipos"></div>
-    </div>
-            
-    <div id="imgpkm">
-        <img src="${imagem}" id="img"/>
-    </div>
-        <hr>
-    <div id="info">
-                
-    </div>
-`;
-let tiposlista = document.querySelector("#tipos");
-tipos.forEach(tipo => {
-    tiposlista.innerHTML += `
-        <img src="./tipos-pkm/${tipo}.svg" id="tipo"/>
-    `;
-});
-let nomepkm = document.querySelector("#nome");
-let imagemindex = document.querySelector("#img");
-let tipopkm = document.querySelector("#tipo");
-let info = document.querySelector("#info");
-info.innerHTML = "";
-
-arraypkm.forEach(item => {
-    info.innerHTML += `
-    <div id="dados">
-        <strong id="nomestat">${item.nomestatus}:</strong>
-        <p id="valorstat">${item.valor}</p>
+        cardpkm.classList.add("card-pokemon");
+        function iniciarAnimacao() {
+            cardpkm.classList.add("giro");
+        }
         
-    </div>
-    <div id="barra" style="width:300px; height:10px; background-color: white;">
-        <div style="width:${item.valor}px; height:10px; background-color: red;">
-    </div>
+        function pararAnimacao() {
+            cardpkm.style.animation = "none"; 
+            cardpkm.offsetHeight; 
+            cardpkm.style.animation = null; 
+        }
+        
+    
+    
+    cardpkm.style.display = "flex";
+    iniciarAnimacao();
+    
+    function renderizaCard() {
+        cardpkm.innerHTML = `
+        <div id="infocima">
+            <p id="nome">${nome}</p>
+            <div id="tipos"></div>
+        </div>
+                
+        <div id="imgpkm">
+            <img src="${imagem}" id="img"/>
+        </div>
+            <hr>
+        <div id="info">
+                    
+        </div>
     `;
-});
+    let tiposlista = document.querySelector("#tipos");
+    tipos.forEach(tipo => {
+        tiposlista.innerHTML += `
+            <img src="./tipos-pkm/${tipo}.svg" id="tipo"/>
+        `;
+    });
+    let nomepkm = document.querySelector("#nome");
+    let imagemindex = document.querySelector("#img");
+    let tipopkm = document.querySelector("#tipo");
+    let info = document.querySelector("#info");
+    info.innerHTML = "";
+    
+    arraypkm.forEach(item => {
+        info.innerHTML += `
+        <div id="dados">
+            <strong id="nomestat">${item.nomestatus}:</strong>
+            <p id="valorstat">${item.valor}</p>
+            
+        </div>
+        <div id="barra" style="width:300px; height:10px; background-color: white;">
+            <div style="width:${item.valor}px; height:10px; background-color: red;">
+        </div>
+        `;
+    });
+    }
+    pararAnimacao();
+    setTimeout(renderizaCard, 200);
+    }
+}catch(erro){
+   console.log("erro: ", erro.message);
 }
-pararAnimacao();
-setTimeout(renderizaCard, 200);
-}
-
 });
-
-
-
